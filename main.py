91d2687e3f5aabc8e31086bfb90102fb32123d8a
LanikTop <russaf05@gmail.com>
2024-01-15 21:54:20 +0500

    Good version THERD

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
+class Template:
+    def __init__(self, screen):
+        uic.loadUi('UI.ui', screen)
+
+
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        Template(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -26,7 +31,7 @@ class Example(QMainWindow):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setPen(QColor(255, 255, 0))
+        qp.setPen(QColor(choice(range(256)), choice(range(256)), choice(range(256))))
         diametr = choice(range(200))
         qp.drawEllipse(choice(range(self.width())), choice(range(self.height())), diametr, diametr)
 
