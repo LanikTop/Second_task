0fe8864d08d97364a682ae5268401cee703b26e8
LanikTop <russaf05@gmail.com>
2024-01-15 21:46:39 +0500

    Good version

--- main.py
+++ main.py
@@ -4,53 +4,6 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>290</x>
-      <y>420</y>
-      <width>221</width>
-      <height>101</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Нажми и создай окружность</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>21</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
-
 
 class Example(QMainWindow):
     def __init__(self):
@@ -63,7 +16,7 @@ class Example(QMainWindow):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            for i in range(choice(range(10))):
+            for i in range(10):
                 self.draw_circle(qp)
             qp.end()
         self.do_paint = False
@@ -73,8 +26,9 @@ class Example(QMainWindow):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(choice(range(800)), choice(range(600)), choice(range(800)), choice(range(600)))
+        qp.setPen(QColor(255, 255, 0))
+        diametr = choice(range(200))
+        qp.drawEllipse(choice(range(self.width())), choice(range(self.height())), diametr, diametr)
 
 
 if __name__ == '__main__':
